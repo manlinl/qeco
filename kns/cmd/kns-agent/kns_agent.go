067527622ffa2699ec282f8aa9cd146b260a873e
Manlin Li <manlinli@gmail.com>
2022-12-26 22:30:50 -0800

    Fix imports ordering in kns_agent.go

--- kns/cmd/kns-agent/kns_agent.go
+++ kns/cmd/kns-agent/kns_agent.go
@@ -5,18 +5,19 @@ import (
 	"flag"
 	"net/netip"
 
-	"qeco.dev/pkg/base"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
 	"k8s.io/klog/v2"
+
 	pb "qeco.dev/apis/kns/v1"
 	"qeco.dev/kns/internal/client"
+	"qeco.dev/pkg/base"
 	_ "qeco.dev/pkg/debug"
 )
 
 var (
-	knsServer = flag.String("kns_server", "localhost:9290", "KNS server address.")
+	knsServer = flag.String("kns_server", ":9290", "KNS server address.")
 	ip        = flag.String("ip", "127.0.0.1", "IP to register with KNS server")
 	address   = flag.String("address", "localhost", "Address used to register with KNS server")
 )
